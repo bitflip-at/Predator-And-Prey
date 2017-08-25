d904d21d54acbeb436490a29e122f15006264419
bitflip-at <emanuel.gitterle@bitflip.at>
2017-08-25 14:43:42 +0200

    first Prototype finished

--- src/at/bitflip/predatorandprey/automata/Automata.java
+++ src/at/bitflip/predatorandprey/automata/Automata.java
@@ -25,6 +25,7 @@ package at.bitflip.predatorandprey.automata;
 
 import at.bitflip.predatorandprey.tile.Tile;
 import at.bitflip.predatorandprey.tile.TileType;
+import at.bitflip.predatorandprey.utils.Pair;
 import java.util.Random;
 
 /**
@@ -32,6 +33,11 @@ import java.util.Random;
  * @author Emanuel Gitterle <emanuel.gitterle@bitflip.at>
  */
 public class Automata {
+
+    private static final int STARTHP = 100;
+    private static final int REPLICATEHP = 200;
+    private static final int PREYPTURN = 10;
+    private static final int PREDPTURN = -1;
 
     private final int sizeX;
     private final int sizeY;
@@ -44,6 +50,7 @@ public class Automata {
     private double predSpawnRate;
 
     private TileType[][] field;
+    private int[][] health;
 
     public Automata(int sizeX, int sizeY, double preySpawnRate, double predSpawnRate) {
         this.sizeX = sizeX;
@@ -92,6 +99,7 @@ public class Automata {
 
     private void initializeField() {
         field = new TileType[sizeX][sizeY];
+        health = new int[sizeX][sizeY];
 
         Random rand = new Random(System.nanoTime());
 
@@ -101,12 +109,15 @@ public class Automata {
 
                 if (r <= preySpawnRate) {
                     field[i][j] = TileType.PREY;
+                    health[i][j] = STARTHP;
                     preyCount++;
                 } else if (r >= (200.0 - predSpawnRate)) {
                     field[i][j] = TileType.PREDATOR;
+                    health[i][j] = STARTHP;
                     predCount++;
                 } else {
                     field[i][j] = TileType.EMPTY;
+                    health[i][j] = -1;
                 }
             }
         }
@@ -114,7 +125,150 @@ public class Automata {
     }
 
     private void updateField() {
+        TileType[][] tmpField = new TileType[sizeX][sizeX];
+        int[][] tmpHealth = new int[sizeX][sizeY];
+        generation++;
 
+        for (int i = 0; i < sizeX; i++) {
+            for (int j = 0; j < sizeY; j++) {
+                tmpField[i][j] = TileType.EMPTY;
+                tmpHealth[i][j] = -1;
+            }
+        }
+
+        for (int i = 0; i < sizeX; i++) {
+            for (int j = 0; j < sizeY; j++) {
+                TileType type = field[i][j];
+                int hp = health[i][j];
+
+                Pair Pos = move(i, j);
+
+                switch (tmpField[i][j]) {
+                    case EMPTY:
+                        tmpField[i][j] = type;
+                        tmpHealth[i][j] = hp;
+                        break;
+                    case PREY:
+                        if (type == TileType.PREY) {
+                            Pair pos = getNextFreePos(i, j);
+                            if (pos != null) {
+                                tmpField[i][j] = type;
+                                tmpHealth[i][j] = hp;
+                            }
+                        } else {
+                            tmpHealth[i][j] += health[i][j];
+                        }
+                        break;
+                    case PREDATOR:
+                        if (type == TileType.PREDATOR) {
+                            Pair pos = getNextFreePos(i, j);
+                            if (pos != null) {
+                                tmpField[i][j] = type;
+                                tmpHealth[i][j] = hp;
+                            }
+                        } else {
+                            tmpHealth[i][j] += health[i][j];
+                        }
+                        break;
+                    default:
+                        tmpField[i][j] = type;
+                        tmpHealth[i][j] = hp;
+                        break;
+                }
+            }
+        }
+
+        health = tmpHealth;
+        field = tmpField;
+
+        // update health and replicate if possible
+        for (int i = 0; i < sizeX; i++) {
+            for (int j = 0; j < sizeY; j++) {
+
+                switch (field[i][j]) {
+                    case PREY:
+                        health[i][j] += PREYPTURN;
+                        break;
+                    case PREDATOR:
+                        health[i][j] += PREDPTURN;
+                        break;
+                }
+
+                if (health[i][j] > REPLICATEHP) {
+                    Pair pos = getNextFreePos(i, j);
+                    if (pos != null) {
+                        field[(int) pos.getX()][(int) pos.getY()] = field[i][j];
+                        health[(int) pos.getX()][(int) pos.getY()] = STARTHP;
+                        health[i][j] -= STARTHP;
+
+                        switch (field[i][j]) {
+                            case PREY:
+                                preyCount++;
+                                break;
+                            case PREDATOR:
+                                predCount++;
+                                break;
+                        }
+                    }
+
+                } else if (health[i][j] < 0) {
+                    health[i][j] = -1;
+
+                    switch (field[i][j]) {
+                        case PREY:
+                            preyCount--;
+                            break;
+                        case PREDATOR:
+                            predCount--;
+                            break;
+                    }
+
+                    field[i][j] = TileType.EMPTY;
+                }
+            }
+        }
+    }
+
+    private Pair getNextFreePos(int i, int j) {
+
+        for (int x = (i - 1); x < (i + 1); x++) {
+            for (int y = (j - 1); y < (j + 1); y++) {
+                if (x < 0) {
+                    continue;
+                }
+                if (y < 0) {
+                    continue;
+                }
+                if (field[x][y] == TileType.EMPTY) {
+                    return new Pair(x, y);
+                }
+            }
+        }
+
+        return null;
+    }
+
+    private Pair move(int x, int y) {
+
+        Random rand = new Random(System.nanoTime());
+        int rX = rand.nextInt(3);
+        int rY = rand.nextInt(3);
+
+        if (rX == 0) {
+            --x;
+        } else if (rX == 2) {
+            ++x;
+        }
+        if (rY == 0) {
+            --y;
+        } else if (rY == 2) {
+            ++y;
+        }
+
+        x = (x + sizeX) % sizeX;
+        y = (y + sizeY) % sizeY;
+
+        return new Pair(x, y);
     }
 
     private Tile[][] Field2Tile() {
